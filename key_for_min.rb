c4f35a43982c2ca2544df93fe95029e9490e5d66
Renee Nordholm <reneenordholm@gmail.com>
2019-04-06 20:51:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,10 @@
 #hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(hash)
-  lowest_value = ""
   hash.collect do |name, number|
     if name == number < 2
-      lowest_value = name
+      return name
     else
       nil
  end
-end
-
- lowest_value
 end
