83c5acb370e545482537ae13e1d1b9e24d8664b6
Renee Nordholm <reneenordholm@gmail.com>
2019-04-06 20:46:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,12 @@
 #hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
+  lowest_value = ""
   name_hash.collect do |name, number|
-    name
+    if name == number
+      lowest_value = name  
+ end
 end
+
+ lowest_value
 end
